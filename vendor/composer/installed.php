aab1bf28647ebcb9e750201da8cb693daf51e8fb
SK <sk@mrvoip.com>
2021-10-04 10:38:33 +0530

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3de77c7bef44acd82783a7cbd247baab502bb759',
+        'name' => 'digitalocean-apps/sample-php',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alrik11es/cowsayphp' => array(
+            'pretty_version' => '1.2.0',
+            'version' => '1.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../alrik11es/cowsayphp',
+            'aliases' => array(),
+            'reference' => '080dc10c8f5420fef35163569293601f17124eeb',
+            'dev_requirement' => false,
+        ),
+        'digitalocean-apps/sample-php' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3de77c7bef44acd82783a7cbd247baab502bb759',
+            'dev_requirement' => false,
+        ),
+    ),
+);
