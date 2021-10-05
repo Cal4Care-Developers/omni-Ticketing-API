feab9d64b5d63f140606655d7f51871f159bf3ae
SK <sk@mrvoip.com>
2021-10-05 12:25:32 +0530

    'test'

--- ms-sso/simplesamlphp/lib/_autoload.php
+++ ms-sso/simplesamlphp/lib/_autoload.php
@@ -13,24 +13,24 @@ declare(strict_types=1);
 // SSP is loaded as a separate project
 
 $test = dirname(dirname(__FILE__)) ;
-//$test=str_replace("workspace/","",$test);
+// $test=str_replace("workspace/","",$test);
 // echo '1212'
 // echo dirname(__FILE__);exit;
-// echo file_exists('/simplesamlphp');exit;
+echo file_exists('/ms-sso');exit;
 // echo file_exists('/ms-sso/simplesamlphp/vendor/autoload.php');exit;
 
-// if (file_exists('/ms-sso/simplesamlphp/vendor/autoload.php')) {
-//     require_once  '/ms-sso/simplesamlphp/vendor/autoload.php';
-// } else {
-    if (file_exists(dirname(dirname(__FILE__)) . '/vendor/autoload.php')) {
-        require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
-    } else {
+if (file_exists('/ms-sso/simplesamlphp/vendor/autoload.php')) {
+    require_once  '/ms-sso/simplesamlphp/vendor/autoload.php';
+} else {
+    // if (file_exists(dirname(dirname(__FILE__)) . '/vendor/autoload.php')) {
+    //     require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
+    // } else {
     // SSP is loaded as a library
     if (file_exists( $test . '/../../../autoload.php')) {
         // echo 'if2';exit;
         require_once  $test . '/../../../autoload.php';
     } else {
-        // echo 'el2';exit;
+       echo 'el2';exit;
         throw new Exception('Unable to load Composer autoloader');
         echo 'el2';
     }
