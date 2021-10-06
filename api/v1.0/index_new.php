a31d1f9d5196a38c8d7d462061d2860dcb103aad
SK <sk@mrvoip.com>
2021-10-06 16:18:01 +0530

    removed to

--- api/v1.0/index_new.php
+++ api/v1.0/index_new.php
@@ -1,18 +1,17 @@
 <?php 
 //echo '13';exit;/print_r($_REQUEST); exit;
 //if($_FILES != '' || $_REQUEST['element_data']['action'] == 'generate_incoming_sms' || $_REQUEST['element_data']['action'] == 'generate_incoming_wp' || $_REQUEST['element_data']['action'] == 'generate_incoming_fb' || $_REQUEST['element_data']['action'] == 'change_wp_status' || $_REQUEST['element_data']['action'] == 'change_wp_status_unoff' || $_REQUEST['element_data']['action'] == 'send_chat_message_media_unoff' || $_REQUEST['element_data']['action'] == 'send_chat_message_unoffs' || $_REQUEST['element_data']['action'] == 'generate_incoming_wp_unoff'){  include_once 'app_redirect.php'; exit; } else { echo 'error'; exit; }
+
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
 header('Content-Type: text/plain; charset=utf-8');
 
-
 if($_FILES != '' || $_REQUEST['element_data']['action'] == 'createExternalTicket' || $_REQUEST['element_data']['action'] == 'createTicketSignature' || $_REQUEST['element_data']['action'] == 'generate_incoming_sms' || $_REQUEST['element_data']['action'] == 'generate_incoming_wp' || $_REQUEST['element_data']['action'] == 'generate_incoming_wp_unoff' || $_REQUEST['element_data']['action'] == 'generate_incoming_fb' || $_REQUEST['element_data']['action'] == 'change_wp_status' || $_REQUEST['element_data']['action'] == 'change_wp_status_unoff' || $_REQUEST['element_data']['action'] == 'send_chat_message_media_unoff' || $_REQUEST['element_data']['action'] == 'send_chat_message_unoffs' || $_REQUEST['element_data']['action'] == 'single_whatsapp_media_upload_uoff' || $_REQUEST['element_data']['action'] == 'send_chat_message_unoff' || $_REQUEST['element_data']['action'] == 'generate_outgoing_wp_unoff' || $_REQUEST['element_data']['action'] == 'generate_incoming_group_wp_unoff' || $_REQUEST['element_data']['action'] == 'generate_incoming_image_wp_unoff' || $_REQUEST['element_data']['action'] == 'generate_incoming_group_image_wp_unoff' || $_REQUEST['element_data']['action'] == 'generate_incoming_message' || $_REQUEST['element_data']['action'] == 'generate_incoming_message_from_outer' || $_REQUEST['element_data']['action'] == 'generate_incoming_message_from_imap' || $_REQUEST['element_data']['action'] == 'sendWhatsappText' || $_REQUEST['element_data']['action'] == 'setWebHook' || $_REQUEST['element_data']['action'] == 'fb_reply_media_upload' || $_REQUEST['element_data']['rootfrom'] == 'mobile_apps' || $_REQUEST['element_data']['action'] == 'composeInternalMail'){ echo 'exit'; include_once 'app_redirect.php';  } else { echo 'error'; exit; }
 
 
 
-require_once 'main.controller.php';
 $admin = new adminData();
 $server_data = $_SERVER;
 $api_req = file_get_contents('php://input');
